9e1c97b90a914f4dea47178d0ee6617020d5e51f
Cristian <cristian__lda@hotmail.com>
2022-07-21 00:14:53 -0400

    arranque con Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_try/app/ui/pages/perfil/perfil_page.dart';
 import 'app/ui/pages/home/home_page.dart';
 //import 'package:flutter_application_try/app/ui/routes/pages.dart';
 //import 'package:flutter_application_try/app/ui/routes/routes.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
